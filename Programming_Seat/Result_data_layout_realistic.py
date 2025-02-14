fad5067b3c3e7037a969610aa33c8e5861fc153b
Discount <33273755+Dis-count@users.noreply.github.com>
2025-02-14 19:51:05 +0800

    update

--- Programming_Seat/Result_data_layout_realistic.py
+++ Programming_Seat/Result_data_layout_realistic.py
@@ -1,44 +1,55 @@
 import numpy as np
 from Comparison import CompareMethods
-
+from Mist import sequence_pool
 # This function is used to record all the data from the simulations.
 # The specific parameters are as follows:
 
 # The layouts are:
-# [0.25, 0.3, 0.25, 0.2] I = 4
-# np.array([17, 18, 19, 20, 21, 21, 22, 23, 24, 25]) given_lines = 10
-# np.ones(given_lines) * 11  given_lines = 20
-# np.ones(given_lines) * 21  given_lines = 10
-# np.ones(given_lines) * 8  given_lines = 15
+
+# [0.25, 0.3, 0.25, 0.2]  I = 4  gamma = 2.4
+
+# np.array([17, 18, 18, 18, 18, 18, 18, 8]) given_lines = 8  
+# length = 133  gap = 39
+
+# np.array([7, 9, 7, 10, 7, 11, 8, 11, 9, 7, 10, 7, 11, 7, 13, 8])  given_lines = 16
+# length = 142  gap = 42
+
+# np.array([3, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13])  given_lines = 13
+#  length = 159  gap = 47
+
+# np.array([6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6])  given_lines = 22
+#  length = 132  gap = 39
+
+# np.array([13, 21, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 21, 13]) 
+# given_lines = 17, length = 367  gap = 108
 
 # output: 
-# data_layout_10: fan-shaped
-# data_layout_20: 20 rows
-# data_layout_15: 15 rows small room
+
 
 if __name__ == "__main__":
     num_sample = 1000  # the number of scenarios
     I = 4  # the number of group types
-    total_period = 50
-    period_range = range(20, total_period, 1)
+    total_period = 120
+    period_range = range(90, total_period, 1)
     # given_lines = 11
 
-    # layout_dic = {'fan': np.array([17, 18, 19, 20, 21, 21, 22, 23, 24, 25]),
-    #               'rec_20rows': np.ones(20) * 11,
-    #               'small': np.ones(15) * 8,
-    #               'rec_10rows': np.ones(10) * 21}
+    # layout_dic = {'HK_FAC': np.array([17, 18, 18, 18, 18, 18, 18, 8]),
+                #   'KTT_TS': np.array([7, 9, 7, 10, 7, 11, 8, 11, 9, 7, 10, 7, 11, 7, 13, 8])}
+    layout_dic = {'NCWCC': np.array([13, 21, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 21, 13])}
 
-    # layout_dic = {'small': np.array([5, 7, 9, 11, 11]),
-    #   'large': np.ones(11) * 15, 'small': np.ones(6) * 7, 'large_not_rec': np.array([15, 15, 15, 15, 15, 15, 17, 17, 17, 17, 17, 17, 17, 17, 17, 19])}
-    layout_dic = {'hk_fac': np.array([17, 18, 18, 18, 18, 18, 18, 8])}
+    # layout_dic = {'KTT_TS': np.array([7, 9, 7, 10, 7, 11, 8, 11, 9, 7, 10, 7, 11, 7, 13, 8]),
+    #               'SWHCC': np.array([6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]),
+    #               'SWCC': np.array([3, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13])
+    #               }
 
     probab = [0.25, 0.3, 0.25, 0.2]
     sd = 1
-    t_value = np.arange(20, total_period, 1)
+    t_value = np.arange(90, total_period, 1)
     people_value = np.zeros(len(period_range))
     occup_value = np.zeros(len(period_range))
     count = 100
-    sequences_pool = np.load('data_sequence0.25.npy')
+    # sequences_pool = np.load('data_sequence0.25.npy')
+    sequences_pool = sequence_pool(count, 120, probab, sd)
 
     for shape, roll_width in layout_dic.items():
         cnt = 0
